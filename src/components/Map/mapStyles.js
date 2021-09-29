8929cdf84a6fc14584cc810eb8a6bc8cc3dea116
chu-young <ttheko101@gmail.com>
2021-09-29 19:14:02 +0200

    added new map style

---
+++ src/components/Map/mapStyles.js
@@ -0,0 +1,98 @@
+export default [
+    {
+        "featureType": "administrative.country",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffdb63"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#aec1ba"
+            },
+            {
+                "lightness": -10
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#1900ff"
+            },
+            {
+                "lightness": -61
+            },
+            {
+                "color": "#dde6a7"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#41cc5a"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ff00d4"
+            },
+            {
+                "lightness": -53
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#91ff00"
+            },
+            {
+                "lightness": -38
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f7a65a"
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#80ff80"
+            },
+            {
+                "lightness": -84
+            },
+            {
+                "weight": 0.1
+            }
+        ]
+    }
+]
